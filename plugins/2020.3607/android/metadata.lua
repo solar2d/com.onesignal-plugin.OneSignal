2c002052c24c5cc801872f4fa736a510bcd4bbd7
Vlad Shcherban <vlad@coronalabs.com>
2020-08-10 19:46:28 +0300

    Updating SDK

---
+++ plugins/2020.3607/android/metadata.lua
@@ -0,0 +1,33 @@
+local metadata =
+{
+   plugin =
+   {
+      format = 'jar',
+      manifest = 
+      {
+         permissions =
+         {
+         },
+         usesPermissions =
+         {
+         },
+         applicationChildElements =
+         {
+          [[
+              <meta-data
+                  android:name="onesignal_app_id"
+                  tools:node="remove" />
+              <meta-data
+                  android:name="onesignal_google_project_number"
+                  tools:node="remove" />
+          ]]
+         },
+      },
+   },
+   coronaManifest = {
+    dependencies = {
+   },
+  },
+}
+
+return metadata
