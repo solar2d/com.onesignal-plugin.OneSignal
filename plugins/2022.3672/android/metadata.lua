7a2133261cb02e6ce82ebc137f705b2ae2651145
Vlad Shcherban <vlad@coronalabs.com>
2022-05-31 22:57:52 +0300

    Updating to latest 4.7.3 for Android to support API32

---
+++ plugins/2022.3672/android/metadata.lua
@@ -0,0 +1,26 @@
+local metadata =
+{
+   plugin =
+   {
+      format = 'jar',
+      manifest = 
+      {
+         permissions =
+         {
+         },
+         usesPermissions =
+         {
+         },
+         applicationChildElements =
+         {
+         },
+      },
+   },
+   coronaManifest = {
+    dependencies = {
+      ["shared.firebase.messaging"] = "com.coronalabs",
+   },
+  },
+}
+
+return metadata
